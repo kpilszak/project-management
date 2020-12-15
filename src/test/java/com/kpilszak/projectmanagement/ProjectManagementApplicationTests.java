0d4862026ffaf0af57acb85aab794b5c9498b4c7
kpilszak <klaudia.pilszak@gmail.com>
2020-12-15 19:42:05 +0100

    Create repo

---
+++ src/test/java/com/kpilszak/projectmanagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
