04361667b5db204c43d6d5450d9fb010d9182748
kpilszak <klaudia.pilszak@gmail.com>
2021-02-09 18:12:32 +0100

    feat: Add list-employees and list-projects views, bind to home view

--- src/main/java/com/kpilszak/projectmanagement/controllers/EmployeeController.java
+++ src/main/java/com/kpilszak/projectmanagement/controllers/EmployeeController.java
@@ -8,12 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 	final EmployeeRepository employeeRepository;
 	
 	public EmployeeController(final EmployeeRepository employeeRepository) {this.employeeRepository = employeeRepository;}
+	
+	@GetMapping
+	public String displayEmployees(Model model) {
+		List<Employee> employees = employeeRepository.findAll();
+		model.addAttribute("employees", employees);
+		return "employees/list-employees";
+	}
 	
 	@GetMapping("/new")
 	public String displayEmployeeForm(Model model) {
--- src/main/java/com/kpilszak/projectmanagement/controllers/ProjectController.java
+++ src/main/java/com/kpilszak/projectmanagement/controllers/ProjectController.java
@@ -1,6 +1,7 @@
 package com.kpilszak.projectmanagement.controllers;
 
 import com.kpilszak.projectmanagement.dao.ProjectRepository;
+import com.kpilszak.projectmanagement.entities.Employee;
 import com.kpilszak.projectmanagement.entities.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
     final ProjectRepository projectRepository;
     
     public ProjectController(final ProjectRepository projectRepository) {this.projectRepository = projectRepository;}
+    
+    @GetMapping
+    public String displayProjects(Model model) {
+        List<Project> projects = projectRepository.findAll();
+        model.addAttribute("projects", projects);
+        return "projects/list-projects";
+    }
     
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
