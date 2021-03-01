9ff4435b338654e50a5ca0e580bd992b9d28aeef
kpilszak <klaudia.pilszak@gmail.com>
2021-03-01 17:35:54 +0100

    feat: Query for Project Status

--- src/main/java/com/kpilszak/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/kpilszak/projectmanagement/controllers/HomeController.java
@@ -1,15 +1,19 @@
 package com.kpilszak.projectmanagement.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kpilszak.projectmanagement.dao.EmployeeRepository;
 import com.kpilszak.projectmanagement.dao.ProjectRepository;
+import com.kpilszak.projectmanagement.dto.ChartData;
 import com.kpilszak.projectmanagement.dto.EmployeeProject;
-import com.kpilszak.projectmanagement.entities.Employee;
 import com.kpilszak.projectmanagement.entities.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -22,9 +26,17 @@ public class HomeController {
 	}
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
+		Map<String, Object> map = new HashMap<>();
+		
 		List<Project> projects = projectRepository.findAll();
 		model.addAttribute("projects", projects);
+		
+		List<ChartData> projectData = projectRepository.getProjectStatus();
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		model.addAttribute("projectStatusCount", jsonString);
 		
 		List<EmployeeProject> employeesProjectCount = employeeRepository.employeeProjects();
 		model.addAttribute("employeesProjectCount", employeesProjectCount);
--- src/main/java/com/kpilszak/projectmanagement/dao/ProjectRepository.java
+++ src/main/java/com/kpilszak/projectmanagement/dao/ProjectRepository.java
@@ -1,6 +1,8 @@
 package com.kpilszak.projectmanagement.dao;
 
+import com.kpilszak.projectmanagement.dto.ChartData;
 import com.kpilszak.projectmanagement.entities.Project;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +10,9 @@ import java.util.List;
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 	@Override
 	List<Project> findAll();
+	
+	@Query(nativeQuery = true, value = "SELECT stage as label, COUNT(*) as value " +
+			                                   "FROM project " +
+			                                   "GROUP BY stage")
+	List<ChartData> getProjectStatus();
 }
