d1ff107778041f1d161aec7c9a9e62c85c6d68ad
kpilszak <klaudia.pilszak@gmail.com>
2021-05-06 17:33:14 +0200

    Configure integration tests

---
+++ src/test/java/com/kpilszak/projectmanagement/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.kpilszak.projectmanagement.dao;
+
+import com.kpilszak.projectmanagement.ProjectManagementApplication;
+import com.kpilszak.projectmanagement.entities.Project;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@ContextConfiguration(classes = ProjectManagementApplication.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql" }),
+		@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")})
+class ProjectRepositoryIntegrationTest {
+	
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	@Test
+	public void ifNewProjectSaved_thenSuccess() {
+		Project project = new Project("New Test Project", "COMPLETE", "Test Description");
+		projectRepository.save(project);
+		
+		assertEquals(5, projectRepository.findAll().size());
+	}
+}
