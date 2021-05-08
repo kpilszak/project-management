dccc610071c682ad63fbc1b94ca60365acaaf7e4
kpilszak <klaudia.pilszak@gmail.com>
2021-05-08 21:19:09 +0200

    Test http request

---
+++ src/test/java/com/kpilszak/projectmanagement/controllers/HttpRequestTest.java
@@ -0,0 +1,28 @@
+package com.kpilszak.projectmanagement.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class HttpRequestTest {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	public void homePageReturnsVersionNumberCorrectly_thenSuccess() {
+		String renderedHtml = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+		assertEquals(renderedHtml.contains("3.0.0"), true);
+	}
+}
