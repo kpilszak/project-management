e933aa10e3195bdc9d336bd9a57c96ed898a1d6a
kpilszak <klaudia.pilszak@gmail.com>
2021-11-30 22:01:39 +0100

    Define around AOP method

--- src/main/java/com/kpilszak/projectmanagement/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/kpilszak/projectmanagement/logging/ApplicationLoggerAspect.java
@@ -1,11 +1,14 @@
 package com.kpilszak.projectmanagement.logging;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -13,13 +16,30 @@ public class ApplicationLoggerAspect {
 	
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	@Pointcut("within(com.kpilszak.projectmanagement.controllers..*) || within(com.kpilszak.projectmanagement.dao..*)")
+	@Pointcut("within(com.kpilszak.projectmanagement.controllers..*)")
 	public void definePackagePointcuts() {
 	}
 	
-	@After("definePackagePointcuts()")
-	public void log() {
-		log.debug(" ------------------------------------------------------------ ");
+	@Around("definePackagePointcuts()")
+	public Object logBefore(ProceedingJoinPoint jp) {
+		log.debug(" \n \n \n ");
+		log.debug("************* Before Method Execution ************* \n {}.{} () with arguments[s] = {}",
+		jp.getSignature().getDeclaringTypeName(), jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("____________________________________________________ \n \n \n");
+		
+		Object o = null;
+		
+		try {
+			o = jp.proceed();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		log.debug("************* After Method Execution ************* \n {}.{} () with arguments[s] = {}",
+		          jp.getSignature().getDeclaringTypeName(), jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("____________________________________________________ \n \n \n");
+		
+		return o;
 	}
 	
 }
