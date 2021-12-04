e9cc3343b534e27fea6fd6f75e88bc975389f83c
kpilszak <klaudia.pilszak@gmail.com>
2021-12-04 20:00:59 +0100

    Spring Security: Authorization In-Memory

--- src/main/java/com/kpilszak/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/com/kpilszak/projectmanagement/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.kpilszak.projectmanagement.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,13 +17,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 				.withUser("myuser")
-				.password("pass")
-				.roles("USER");
+					.password("pass")
+					.roles("USER")
+				.and()
+				.withUser("taz")
+					.password("pass2")
+					.roles("USER")
+				.and()
+				.withUser("managerUser")
+					.password("pass3")
+					.roles("ADMIN");
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/projects/new").hasRole("ADMIN")
+				.antMatchers("/").authenticated().and().formLogin();
 	}
 	
 }
