be31cbf94702ad0e44044458dfb8265174da5067
kpilszak <klaudia.pilszak@gmail.com>
2021-12-05 14:28:56 +0100

    Spring Security: JDBC backed security

--- src/main/java/com/kpilszak/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/com/kpilszak/projectmanagement/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.kpilszak.projectmanagement.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,24 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("myuser")
-					.password("pass")
-					.roles("USER")
-				.and()
-				.withUser("taz")
-					.password("pass2")
-					.roles("USER")
-				.and()
-				.withUser("managerUser")
-					.password("pass3")
-					.roles("ADMIN");
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled " +
+						                     "from users where username = ?")
+				.authoritiesByUsernameQuery("select username, authority " +
+						                           "from authorities where username = ?");
 	}
 	
 	@Bean
@@ -38,7 +37,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/projects/new").hasRole("ADMIN")
+				.antMatchers("/projects/new").hasRole("ADMIN")
+				.antMatchers("/h2_console/**").permitAll()
 				.antMatchers("/").authenticated().and().formLogin();
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 	}
 	
 }
